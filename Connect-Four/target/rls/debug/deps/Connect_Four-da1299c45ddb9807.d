a8a8927eb17a5c9953cf5ed539426d9ff0a67273
Kai Bailey <kbailey1@ualberta.ca>
2020-03-06 10:57:10 -0700

    Initialized Rust Crate

---
+++ Connect-Four/target/rls/debug/deps/Connect_Four-da1299c45ddb9807.d
@@ -0,0 +1,5 @@
+/Users/kai/Documents/class/year4/SoftwareDomains/Connect-Four/Connect-Four/target/rls/debug/deps/Connect_Four-da1299c45ddb9807.rmeta: src/main.rs
+
+/Users/kai/Documents/class/year4/SoftwareDomains/Connect-Four/Connect-Four/target/rls/debug/deps/Connect_Four-da1299c45ddb9807.d: src/main.rs
+
+src/main.rs:
