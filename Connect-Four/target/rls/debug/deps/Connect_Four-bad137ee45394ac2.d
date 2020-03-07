43ff606fd21048661bcf5326cc393e083301f411
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2020-03-06 20:09:00 -0700

    fix gitignore

--- Connect-Four/target/rls/debug/deps/Connect_Four-bad137ee45394ac2.d
+++
@@ -1,5 +0,0 @@
-/Users/kai/Documents/class/year4/SoftwareDomains/Connect-Four/Connect-Four/target/rls/debug/deps/Connect_Four-bad137ee45394ac2.rmeta: src/main.rs
-
-/Users/kai/Documents/class/year4/SoftwareDomains/Connect-Four/Connect-Four/target/rls/debug/deps/Connect_Four-bad137ee45394ac2.d: src/main.rs
-
-src/main.rs:
