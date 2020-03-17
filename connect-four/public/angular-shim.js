260d7ec23e94f5107e4ad2d6c0837b543b5803e4
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2020-03-17 11:57:35 -0600

    remove unused property

--- connect-four/public/angular-shim.js
+++ connect-four/public/angular-shim.js
@@ -13,7 +13,6 @@
                     fn(...args);
                 } else {
                     myDelayedEvents.push({
-                        event: type,
                         fn: fn,
                         args: args,
                     });
