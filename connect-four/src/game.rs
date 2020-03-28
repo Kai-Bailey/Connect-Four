128eb6a3a24a9a60cc92a88c438a91ed10f2d2af
dinulade101 <dinulasd@gmail.com>
2020-03-28 15:51:36 -0600

    working verison

--- connect-four/src/game.rs
+++ connect-four/src/game.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
 use mongodb::bson;
+use chrono::{DateTime, Utc};
 
 // follow this for deserializng datetime - https://serde.rs/custom-date-format.html
 
@@ -12,6 +13,8 @@ pub struct Game {
     pub Player1Name: String,
     pub Player2Name: String,
     pub WinnerName: String,
+//    #[serde(with = "utc_date_format")]
+//    pub GameDate: DateTime<Utc>,
     pub GameDate: bson::UtcDateTime,
 }
 
@@ -22,6 +25,28 @@ pub struct InsertableGame {
     pub Player1Name: String,
     pub Player2Name: String,
     pub WinnerName: String,
-    pub GameDate: bson::UtcDateTime,
+//    #[serde(with = "utc_date_format")]
+//    pub GameDate: DateTime<Utc>,
+    pub GameDate: bson::UtcDateTime
 }
 
+mod utc_date_format {
+    use chrono::{DateTime, Utc, TimeZone};
+    use serde::{self, Deserialize, Serializer, Deserializer};
+
+    const FORMAT: &'static str = "%Y-%m-%dT%H:%M:%S%.3fZ";
+//    "2020-03-24T17:59:10.412Z"
+
+    pub fn serialize<S>(date: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer
+    {
+        let s = format!("{}", date.format(FORMAT));
+        serializer.serialize_str(&s)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+        where D: Deserializer<'de> {
+        let s = String::deserialize(deserializer)?;
+        Utc.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
+    }
+}
