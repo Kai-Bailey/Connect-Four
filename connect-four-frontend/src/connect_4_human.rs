72a7bc8ab64078d28131e25c14fd66611b1a91f7
Kai Bailey <kbailey1@ualberta.ca>
2020-04-04 12:40:58 -0600

    Add 4 pages and routing to frontend

--- connect-four-frontend/src/connect_4_human.rs
+++ connect-four-frontend/src/connect_4_human.rs
@@ -1,348 +1,163 @@
-pub fn main() {
-    // Connect4Human.js
-    js! {
-        angular.module("Connect4App").controller("humanController", humanController);
+use yew::{prelude::*, virtual_dom::VNode, Properties};
 
-        app.factory("postService", function(S_resource) {
-            return S_resource("/games");
-        });
+pub struct Connect4HumanModel {
+    link: ComponentLink<Self>,
+    gameStarted: bool,
+    player1Name: String,
+    player2Name: String,
+}
 
-        function humanController(postService, S_scope, S_rootScope) {
-            S_scope.games=[];
-            S_scope.newGame= {gameNumber:"100", gameType:"Connect-4", Player1Name: "", Player2Name: "", WinnerName: "Unfinished Game", GameDate: Date.now()};
-            Array.prototype.clone = function () {
-                var arr = [], i;
-                for (i = 0; i < this.length; i++) {
-                    arr[i] = this[i].slice();
+use stdweb::traits::*;
+use stdweb::unstable::TryInto;
+use stdweb::web::{document, window, CanvasRenderingContext2d};
+
+use stdweb::web::event::{ClickEvent, ResizeEvent};
+
+use stdweb::web::html_element::CanvasElement;
+
+macro_rules! enclose {
+    ( ($( $x:ident ),*) $y:expr ) => {
+        {
+            $(let $x = $x.clone();)*
+            $y
+        }
+    };
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct Props {}
+
+pub enum Msg {
+    gotPlayer1Name(String),
+    gotPlayer2Name(String),
+    startGame,
+}
+
+fn draw_board() {
+    js! {// draw the mask
+        // http://stackoverflow.com/questions/6271419/how-to-fill-the-opposite-shape-on-canvas
+        // -->  http://stackoverflow.com/a/11770000/917957
+        var canvas = document.getElementsByTagName("canvas")[0];
+        var context = canvas.getContext("2d");
+        context.save();
+        context.fillStyle = "#00bfff";
+        context.beginPath();
+        var x, y;
+        for (y = 0; y < 6; y++) {
+            for (x = 0; x < 7; x++) {
+                context.arc(75 * x + 100, 75 * y + 50, 25, 0, 2 * Math.PI);
+                context.rect(75 * x + 150, 75 * y, -100, 100);
+            }
+        }
+        context.fill();
+        context.restore();
+    }
+}
+
+impl Component for Connect4HumanModel {
+    type Message = Msg;
+    type Properties = Props;
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Connect4HumanModel {
+            link,
+            gameStarted: false,
+            player1Name: "".to_string(),
+            player2Name: "".to_string(),
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::gotPlayer1Name(newName) => {
+                self.player1Name = newName;
+            }
+            Msg::gotPlayer2Name(newName) => {
+                self.player2Name = newName;
+            }
+            Msg::startGame => {
+                self.gameStarted = true;
+                draw_board();
+            }
+        }
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn mounted(&mut self) -> ShouldRender {
+        let canvas: CanvasElement = document()
+            .query_selector("#gameboard")
+            .unwrap()
+            .unwrap()
+            .try_into()
+            .unwrap();
+        let context: CanvasRenderingContext2d = canvas.get_context().unwrap();
+
+        canvas.set_width(canvas.offset_width() as u32);
+        canvas.set_height(canvas.offset_height() as u32);
+
+        window().add_event_listener(enclose!( (canvas) move |_: ResizeEvent| {
+            canvas.set_width(canvas.offset_width() as u32);
+            canvas.set_height(canvas.offset_height() as u32);
+        }));
+
+        let rect = canvas.get_bounding_client_rect();
+
+        canvas.add_event_listener(enclose!( (context) move |event: ClickEvent| {
+            let x_click = event.client_x() - rect.get_left() as i32;
+            let y_click = event.client_y() - rect.get_top() as i32;
+
+            for col in 0..7 {
+                let x_col = 75 * col + 100;
+                if (x_click - x_col) * (x_click - x_col) <= 25 * 25 {
+                    js! {
+                        alert( @{col} );
+                    };
                 }
-                return arr;
-            };
-            S_scope.Game=function(){
-                var target = document.getElementById("gameboard");
-                var button = document.getElementById("startbutton");
-                button.disabled = true;
+            }
+        }));
 
-                var target1 = document.getElementById("textbox1");
-                var target2 = document.getElementById("textbox2");
+        false
+    }
 
-                var that = this;
-                this.map = [];
-                this.paused = false;
-                this.won = false;
-                this.rejectClick = false;
-                this.move = 0;
-                this.aiHistory = [];
-                this.initOnceDone = false;
-                this.initOnce = function () {
-                if (this.initOnceDone) {
-                    return false;
-                }
+    fn view(&self) -> VNode {
+        let title;
+        if self.gameStarted {
+            title = "Human VS Human Connect 4";
+        } else {
+            title = "Enter Player Names";
+        }
 
-                this.canvas = document.getElementsByTagName("canvas")[0];
-                this.canvas.addEventListener("click", function (e) {
-                    that.onclick(that.canvas, e);
-                });
-                this.context = this.canvas.getContext("2d");
-                this.initOnceDone = true;
-            };
-
-        this.init = function () {
-                this.map = [];
-                this.paused = false;
-                this.won = false;
-                this.rejectClick = false;
-                this.move = 0;
-                this.aiHistory = [];
-                this.initOnce();
-
-                var i, j;
-                for (i = 0; i <= 6; i++) {
-                    this.map[i] = [];
-                    for (j = 0; j <= 7; j++) {
-                        this.map[i][j] = 0;
+        html! {
+        <div id="main" ng-controller="humanController">
+            <div class="w3-container" id="services" style="margin-top:75px">
+               <h5 class="w3-xxxlarge w3-text-red"><b>{title}</b></h5>
+               <hr style="width:50px;border:5px solid red" class="w3-round"/>
+            </div>
+            <div class="col-md-offset-4 col-md-8">
+               { if self.gameStarted {
+                    html! {
+                    <div>
+                        <h4>{"New Game: "} {&self.player1Name} {" VS "} {&self.player2Name}</h4>
+                        <small>{"Disc Colors: "} {&self.player1Name} {" - Red    and    "} {&self.player2Name} {" - Yellow"}</small>
+                     </div>
                     }
-                }
-                this.clear();
-                this.drawMask();
-                this.print();
-            };
-
-            this.playerMove = function () {
-                if (this.move % 2 === 0) {
-                    return 1;
-                }
-                return -1;
-            };
-
-            this.print = function () {
-                var i, j, msg;
-                msg = "\n";
-                msg += "Move: " + this.move;
-                msg += "\n";
-                for (i = 0; i < 6; i++) {
-                    for (j = 0; j < 7; j++) {
-                        msg += " " + this.map[i][j];
-                    }
-                    msg += "\n";
-                }
-                console.log(msg);
-            };
-
-            this.printState = function (state) {
-                var i, j, msg = "\n";
-                for (i = 0; i < 6; i++) {
-                    for (j = 0; j < 7; j++) {
-                        msg += " " + state[i][j];
-                    }
-                    msg += "\n";
-                }
-                console.log(msg);
-            };
-
-            this.win = function (player) {
-                this.paused = true;
-                this.won = true;
-                this.rejectClick = false;
-                var msg = null;
-                if (player > 0) {
-                    msg = S_scope.newGame.Player1Name + " wins";
-                    S_scope.newGame.WinnerName=S_scope.newGame.Player1Name;
-                } else if (player < 0) {
-                    msg = S_scope.newGame.Player2Name + " wins";
-                    S_scope.newGame.WinnerName=S_scope.newGame.Player2Name;
-                } else {
-                    msg = "It's a draw";
-                    S_scope.newGame.WinnerName="Draw";
-                }
-                msg += " - Click on game board to reset";
-                this.context.save();
-                this.context.font = "14pt sans-serif";
-                this.context.fillStyle = "#111";
-                this.context.fillText(msg, 150, 20);
-
-                postService.save(S_scope.newGame, function(){
-
-                    console.log("succesfully saved");
-                });
-
-                this.canvas = document.getElementsByTagName("canvas")[0];
-                this.canvas.addEventListener("click", function (e) {
-                    location.reload();
-                });
-                button.disabled = false;
-
-                console.info(msg);
-            };
-            this.fillMap = function (state, column, value) {
-                var tempMap = state.clone();
-                if (tempMap[0][column] !== 0 || column < 0 || column > 6) {
-                    return -1;
-                }
-
-                var done = false,
-                    row = 0,
-                    i;
-                for (i = 0; i < 5; i++) {
-                    if (tempMap[i + 1][column] !== 0) {
-                        done = true;
-                        row = i;
-                        break;
-                    }
-                }
-                if (!done) {
-                    row = 5;
-                }
-                tempMap[row][column] = value;
-                return tempMap;
-
-            };
-
-            this.action = function (column, callback) {
-                if (this.paused || this.won) {
-                    return 0;
-                }
-                if (this.map[0][column] !== 0 || column < 0 || column > 6) {
-                    return -1;
-                }
-
-                var done = false;
-                var row = 0, i;
-                for (i = 0; i < 5; i++) {
-                    if (this.map[i + 1][column] !== 0) {
-                        done = true;
-                        row = i;
-                        break;
-                    }
-                }
-                if (!done) {
-                    row = 5;
-                }
-                this.animate(column, this.playerMove(this.move), row, 0, function () {
-                    that.map[row][column] = that.playerMove(that.move);
-                    that.move++;
-                    that.draw();
-                    that.check();
-                    that.print();
-                });
-                this.paused = true;
-                return 1;
-            };
-
-            this.check = function () {
-                var i, j, k;
-                var temp_r = 0, temp_b = 0, temp_br = 0, temp_tr = 0;
-                for (i = 0; i < 6; i++) {
-                    for (j = 0; j < 7; j++) {
-                        temp_r = 0;
-                        temp_b = 0;
-                        temp_br = 0;
-                        temp_tr = 0;
-                        for (k = 0; k <= 3; k++) {
-                            // from (i,j) to right
-                            if (j + k < 7) {
-                                temp_r += this.map[i][j + k];
-                            }
-                            // from (i,j) to bottom
-                            if (i + k < 6) {
-                                temp_b += this.map[i + k][j];
-                            }
-
-                            // from (i,j) to bottom-right
-                            if (i + k < 6 && j + k < 7) {
-                                temp_br += this.map[i + k][j + k];
-                            }
-
-                            // from (i,j) to top-right
-                            if (i - k >= 0 && j + k < 7) {
-                                temp_tr += this.map[i - k][j + k];
-                            }
-                        }
-                        if (Math.abs(temp_r) === 4) {
-                            this.win(temp_r);
-                        } else if (Math.abs(temp_b) === 4) {
-                            this.win(temp_b);
-                        } else if (Math.abs(temp_br) === 4) {
-                            this.win(temp_br);
-                        } else if (Math.abs(temp_tr) === 4) {
-                            this.win(temp_tr);
-                        }
-
-                    }
-                }
-                // check if draw
-                if ((this.move === 42) && (!this.won)) {
-                    this.win(0);
-                }
-            };
-
-            this.drawCircle = function (x, y, r, fill, stroke) {
-                this.context.save();
-                this.context.fillStyle = fill;
-                this.context.strokeStyle = stroke;
-                this.context.beginPath();
-                this.context.arc(x, y, r, 0, 2 * Math.PI, false);
-                this.context.fill();
-                this.context.restore();
-            };
-            this.drawMask = function () {
-                // draw the mask
-                // http://stackoverflow.com/questions/6271419/how-to-fill-the-opposite-shape-on-canvas
-                // -->  http://stackoverflow.com/a/11770000/917957
-
-                this.context.save();
-                this.context.fillStyle = "#00bfff";
-                this.context.beginPath();
-                var x, y;
-                for (y = 0; y < 6; y++) {
-                    for (x = 0; x < 7; x++) {
-                        this.context.arc(75 * x + 100, 75 * y + 50, 25, 0, 2 * Math.PI);
-                        this.context.rect(75 * x + 150, 75 * y, -100, 100);
-                    }
-                }
-                this.context.fill();
-                this.context.restore();
-            };
-
-            this.draw = function () {
-                var x, y;
-                var fg_color;
-                for (y = 0; y < 6; y++) {
-                    for (x = 0; x < 7; x++) {
-                        fg_color = "transparent";
-                        if (this.map[y][x] >= 1) {
-                            fg_color = "#ff4136";
-                        } else if (this.map[y][x] <= -1) {
-                            fg_color = "#ffff00";
-                        }
-                        this.drawCircle(75 * x + 100, 75 * y + 50, 25, fg_color, "black");
-                    }
-                }
-            };
-            this.clear = function () {
-                this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-            };
-            this.animate = function (column, move, to_row, cur_pos, callback) {
-                var fg_color = "transparent";
-                if (move >= 1) {
-                    fg_color = "#ff4136";
-                } else if (move <= -1) {
-                    fg_color = "#ffff00";
-                }
-                if (to_row * 75 >= cur_pos) {
-                    this.clear();
-                    this.draw();
-                    this.drawCircle(75 * column + 100, cur_pos + 50, 25, fg_color, "black");
-                    this.drawMask();
-                    window.requestAnimationFrame(function () {
-                        that.animate(column, move, to_row, cur_pos + 25, callback);
-                    });
-                } else {
-                    callback();
-                }
-            };
-
-            this.onregion = function (coord, x, radius) {
-                if ((coord[0] - x)*(coord[0] - x) <=  radius * radius) {
-                    return true;
-                }
-                return false;
-            };
-            this.oncircle = function (coord, centerCoord, radius) {
-                if ((coord[0] - centerCoord[0]) * (coord[0] - centerCoord[0]) <=  radius * radius
-                        && (coord[1] - centerCoord[1]) * (coord[1] - centerCoord[1]) <=  radius * radius) {
-                    return true;
-                }
-                return false;
-            };
-
-            this.onclick = function (canvas, e) {
-                if (this.rejectClick) {
-                    return false;
-                }
-                if (this.won) {
-                    this.init();
-                    return false;
-                }
-                var rect = canvas.getBoundingClientRect(),
-                    x = e.clientX - rect.left, // - e.target.scrollTop,
-                    y = e.clientY - rect.top;  // - e.target.scrollLeft;
-
-                var j, valid;
-                for (j = 0; j < 7; j++) {
-                    if (this.onregion([x, y], 75 * j + 100, 25)) {
-                        this.paused = false;
-                        this.action(j);
-                        if (valid === 1) { // give user retry if action is invalid
-                            this.rejectClick = true;
-                        }
-                        break; // because there will be no 2 points that are clicked at a time
-                    }
-                }
-            };
-
-            this.init();
-                S_scope.games.push(S_scope.newGame);
-            };
+               } else {
+                html!{
+                    <div class="col-md-offset-3 col-md-8">
+                        <input id="textbox1" type="text" placeholder="Player 1's Name" oninput=self.link.callback(|e: InputData| Msg::gotPlayer1Name(e.value))/>
+                        <input id="textbox2" type="text" placeholder="Player 2's Name" oninput=self.link.callback(|e: InputData| Msg::gotPlayer2Name(e.value))/>
+                        <button onclick=self.link.callback(|_| Msg::startGame)>{ "Start Game" }</button>
+                    </div>
+                  }
+                 }
+               }
+               <canvas id="gameboard" height="480" width="640"></canvas>
+            </div>
+         </div>
         }
     }
 }
