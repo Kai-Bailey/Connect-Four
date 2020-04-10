131fce9a6c1670f9c59ec3fce436dc4209208c37
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2020-04-09 19:40:23 -0600

    main.rs ready

--- connect-four-cli/src/main.rs
+++ connect-four-cli/src/main.rs
@@ -127,26 +127,40 @@ impl TootOttoGameEvents for TootOttoCliInterface {
     }
 
     fn player_turn(&self, col_size: usize) -> Result<(TootOttoChipType, usize), ()> {
+        let chip_type;
+
+        println!("Please select a chip type (T or O)");
+        let chip = retrieve_user_input();
+        if chip.is_ok() {
+            let chip_str = chip.unwrap();
+            if chip_str == "T" {
+                chip_type = TootOttoChipType::T;
+            } else if chip_str == "O" {
+                chip_type = TootOttoChipType::O;
+            } else {
+                return Err(())
+            }
+        }
+
         println!("Please select a column (0-{})", col_size - 1);
         let col = retrieve_user_input();
         if col.is_ok() {
-            let input = col.unwrap().chars();
-            let chip = input.next().unwrap();
-
-            let chip_type;
-            if chip == 'T' {
-                chip_type = TootOttoChipType::T;
-            } else {
-                chip_type = TootOttoChipType::O;
-            }
-
-            return Ok(col.unwrap().parse().unwrap());
+            return Ok((chip_type, col.unwrap().parse().unwrap()));
         }
         return Err(());
     }
 
-    fn selected_column(&self, player: String, col: usize) {
-        println!("{} Selected Column {}", player, col)
+    fn selected_column(&self, player: String, chip_type: TootOttoChipType, col: usize) {
+        let chip_str;
+        match chip_type {
+            TootOttoChipType::T => {
+                chip_str = "T";
+            },
+            TootOttoChipType::O => {
+                chip_str = "O";
+            },
+        };
+        println!("{} Selected Chip Type {} and Column {}", player, chip_str, col);
     }
 
     fn animate_chip(&self) {}
@@ -254,10 +268,104 @@ fn start_connect_four() {
         }
     }
 
-    let handler: CliInterface = CliInterface {};
+    let handler: ConnectFourCliInterface = ConnectFourCliInterface {};
     game.start_game_cli(handler);
 }
 
 fn start_toot_and_otto() {
-    // TODO
+    println!("Select a game board size for Toot-Otto: ");
+    // Size variations include 5×4, 6×5, 8×7, 9×7, 10×7, 8×8,
+    println!("1) 6 x 7");
+    println!("2) 5 x 4");
+    println!("3) 6 x 5");
+    println!("4) 8 x 7");
+    println!("5) 9 x 7");
+    println!("6) 10 x 7");
+    println!("7) 8 x 8");
+
+    let sel = retrieve_user_input();
+    let num_rows;
+    let num_cols;
+    match sel {
+        Ok(x) => match x.as_str() {
+            "1" => {
+                num_rows = 6;
+                num_cols = 7;
+            }
+            "2" => {
+                num_rows = 5;
+                num_cols = 4;
+            }
+            "3" => {
+                num_rows = 6;
+                num_cols = 5;
+            }
+            "4" => {
+                num_rows = 8;
+                num_cols = 7;
+            }
+            "5" => {
+                num_rows = 9;
+                num_cols = 7;
+            }
+            "6" => {
+                num_rows = 10;
+                num_cols = 7;
+            }
+            "7" => {
+                num_rows = 8;
+                num_cols = 8;
+            }
+
+            _ => {
+                println!("Invalid input");
+                return;
+            }
+        },
+        Err(_) => {
+            println!("Invalid input");
+            return;
+        }
+    }
+
+    println!("Do you want to play against an AI or another human?");
+    println!("1) Human");
+    println!("2) AI");
+
+    let sel = retrieve_user_input();
+    let mut game;
+
+    match sel {
+        Ok(x) => match x.as_str() {
+            "1" => {
+                game = TootOttoGame::new(
+                    num_rows,
+                    num_cols,
+                    false,
+                    "P1".to_string(),
+                    "P2".to_string(),
+                );
+            }
+            "2" => {
+                game = TootOttoGame::new(
+                    num_rows,
+                    num_cols,
+                    true,
+                    "Player".to_string(),
+                    "Computer".to_string(),
+                );
+            }
+            _ => {
+                println!("Invalid input");
+                return;
+            }
+        },
+        Err(_) => {
+            println!("Invalid input");
+            return;
+        }
+    }
+
+    let handler: TootOttoCliInterface = TootOttoCliInterface {};
+    game.start_game_cli(handler);
 }
